62420c552b9c4872594e954227f455e78a237ed5
julianvazq <50368117+julianvazq@users.noreply.github.com>
2019-08-02 19:22:20 -0400

    Add smooth transitions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Web project box hover effects on mobile (screen width < 900)
 if (document.documentElement.clientWidth < 900) {
   document
     .querySelector(".web-projects")
@@ -10,14 +11,37 @@ if (document.documentElement.clientWidth < 900) {
     });
 
   document.addEventListener("click", function(e) {
-    //   console.log(e.target.parentElement);
-    if (e.target.parentElement.className !== "web-project-box") {
+    if (
+      e.target.parentElement != null &&
+      e.target.parentElement.className !== "web-project-box"
+    ) {
       let figcaps = document.querySelectorAll("figcaption");
-      console.log(figcaps);
       for (let i = 0; i < figcaps.length; i++) {
         figcaps[i].classList.remove("fig-hover-effect");
         figcaps[i].children[0].classList.remove("fig-hover-effect");
       }
+    }
+  });
+}
+
+if (document.documentElement.clientWidth < 636) {
+  //Changes hamburger icon to a cross if checkbox is checked
+  document
+    .querySelector("input[name=checkbox]")
+    .addEventListener("change", function() {
+      console.log("changed.");
+      if (this.checked) {
+        document.querySelector("nav label").innerHTML =
+          '<i class="fas fa-times"></i>';
+      }
+    });
+
+  //Changes icon back to hamburguer on mouseup anywhere on the page but on the icon
+  document.addEventListener("mouseup", function(e) {
+    if (document.querySelector("nav input").checked) {
+      document.querySelector("nav input").checked = false;
+      document.querySelector("nav label").innerHTML =
+        '<i class="fas fa-bars"></i>';
     }
   });
 }
